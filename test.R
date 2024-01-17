c53442d57849026b45173f1db8e4a78b23c788cc
nikkiyu04 <nikki.yu04@gmail.com>
2024-01-17 22:43:09 +0000

    test file

---
+++ test.R
@@ -0,0 +1,5 @@
+library(haven)
+individual_IR <- read_dta("~/Downloads/UNICEF DATA/Individual/TZIR82FL.DTA")
+household_member_PR <- read_dta("~/Downloads/UNICEF DATA/Household Member/TZPR82FL.DTA")
+
+
