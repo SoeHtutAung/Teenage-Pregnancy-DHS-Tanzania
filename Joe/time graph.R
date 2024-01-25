f94f648ecad6cb4d99b7dbaf19c84174f94be9d7
joestreets149 <147051520+joestreets149@users.noreply.github.com>
2024-01-25 19:13:41 +0000

    Update time graph.R
    Adding some comments to the complex ggplot object

--- Joe/time graph.R
+++ Joe/time graph.R
@@ -3,7 +3,7 @@ library(tidyr)
 
 ### This is the code I used to generate the graph on our midway presentation
 
-# data taken manually from STATcompiler
+# data taken manually from STATcompiler tables
 # https://www.statcompiler.com/en/
 tzn_nmr <- data.frame(
   Year = c(1992, 1996, 1999, 2004, 2007, 2010, 2015, 2022),
@@ -15,23 +15,24 @@ tzn_nmr <- data.frame(
   urban_h = c(83, 37, 84, 54, 60, 47, 49, 46)
 )
 
+# make objects for lower and upper bounds of the CIs
 lowersh <- c(tzn_nmr$rural_l, tzn_nmr$urban_l)
 uppersh <- c(tzn_nmr$rural_h, tzn_nmr$urban_h)
 
 tzn_nmr %>%
-  gather(Residence, value, rural, urban) %>%
-  ggplot( aes( x=Year , y=value , color=Residence ) ) +
-  geom_point( pch=17 , size=2 ) +
+  gather(Residence, value, rural, urban) %>% # not 100% what 'gather' does, but it works
+  ggplot( aes( x=Year , y=value , color=Residence ) ) + 
+  geom_point( pch=17 , size=2 ) + # triangles
   geom_line( linewidth=1 , linetype="dotted" ) +
-  geom_ribbon(
+  geom_ribbon( # ribbons create upper and lower lines and fill them in
     aes( ymin=lowersh , ymax=uppersh , fill=Residence ) ,
     linetype=0 ,
     alpha=0.25 ) +
-  theme_bw() + 
-  scale_color_manual( values = c("darkorange","blue") ) +
+  theme_bw() + # classic background
+  scale_color_manual( values = c("darkorange","blue") ) + # this is important when you have two different lines on the same plot
   theme( legend.position = c(0.85,0.8) ,
          plot.title = element_text(hjust = 0.5),
          plot.subtitle = element_text(hjust = 0.5) ) +
-  xlim( 1992,2022 ) + ylim( 0,100 ) +
+  xlim( 1992,2022 ) + ylim( 0,100 ) + # changing the limits
   ylab( "NMR (per 1,000 live births)" ) +
-  ggtitle("Neonatal Mortality Rates in Tanzania","by Place of Residence")
+  ggtitle("Neonatal Mortality Rates in Tanzania","by Place of Residence") # arg1 Title, arg2 Subtitle
