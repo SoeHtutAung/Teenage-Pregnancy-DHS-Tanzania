97bef7e6d89db0fdf5493b713fc1977095a0e353
nikkiyu04 <nikki.yu04@gmail.com>
2024-01-31 21:07:54 +0000

    new gis file

---
+++ nm stuff/rates_gis.R
@@ -0,0 +1,155 @@
+library(dplyr)
+library(haven)
+library(survey)
+library(tidyverse)
+library(plyr)
+
+
+#######LOAD DATA
+##read births data set
+births_BR <- read_dta("TZBR82FL.DTA")
+births_BR <- read_dta("~/Downloads/UNICEF DATA/Births/TZBR82FL.DTA") # for nikki 
+
+##load txt file with variable codes from code list
+NM_variables <- readLines("NM_variables.txt")
+NM_variables <- readLines("/Users/nikkiyu/Downloads/2 Data Challenge/unicef/nm stuff/NM_variables.txt") #for nikki
+
+##FILTER DATA BY VARIABLE CODES
+births_subset <- births_BR %>% select(all_of(NM_variables)) %>% mutate(v024 = births_BR$v024)
+
+##CREATE NEW VARS
+##mum_age_pregancy = mum dob - baby dob
+births_subset$mum_age_pregnancy<- round(((births_subset$b3 - births_subset$v011)/12),0)
+
+##how many years ago from interview was the child born?
+## date of interview - date of birth of child
+births_subset$test <- round(((births_subset$v008 - births_subset$b3)/12),0)
+
+##FILTER BY BIRTHS IN LAST 3 YRS
+##filter test column to < 3 to use only births in the lat 3 years
+births_last3years <- births_subset %>% filter(test < 3)
+
+##summary
+summary(births_last3years)
+
+##I used the summary to reduce the variable list and altered the txt file accordingly
+##I kept BMI(v445) (even though 2782 records missing BMI
+## Kept Hb/anaemia level(v456/v457) as we know not all women were checked
+## m70 from NR (baby postnatal checked within 2 months - maybe not so useful as neo_mort defined as death within 1 month)
+## m70 and m66 are regarding health checks for mother and baby were well completed
+## however the other variables related to health checks (time and person who did check) had >50% missingness
+
+##NEW VAR FOR NEONATAL MORTALITY
+##create new column to indicate if neonatal death (130 is 30 days, so if b6 <= 130 it will be 1)
+## the second line gives the age in days of the baby when it died
+births_last3years <- births_last3years %>%
+  mutate(neo_mort = ifelse(is.na(b6) | b6 > 130, "No", "Yes"),
+         age_at_death_days = ifelse(neo_mort == "Yes", b6 %% 100, NA))
+
+##RECODE DELIVERY ASSISTANT COLUMNS
+##combine the columns m3a-m3n (assistant at delivery) into one column with factors
+##From DHS statistics manual - 
+##During data collection respondents may mention more than one provider. The percent distribution by type of provider takes the highest type of provider from the list above and does not include other providers mentioned by the respondent
+## re coded as per stats manual although these columns vary by country and not sure where to put mch aide
+##currently put if (m3f) in with auxillery nurse as it seems like a community clinical role
+
+births_last3years <- births_last3years %>%
+  mutate(
+    senior_delivery_attendant = case_when(
+      m3a == 1 ~ "Doctor",
+      m3b == 1 | m3d == 1 | m3e == 1 ~ "Nurse/midwife",
+      m3c == 1 | m3f ==1  ~ "Auxiliary midwife",
+      m3g == 1 ~ "Traditional birth attendant",
+      m3h == 1 | m3i == 1 | m3k == 1 ~ "Relative/other",
+      m3n == 1 ~ "No one",
+      TRUE ~ NA_character_
+    )
+  )
+
+##RECODE PLACE OF DELIVERY AS PER DHS MANUAL
+births_last3years <- births_last3years %>%
+  mutate(m15 = case_when(
+    m15 %in% 20:29 ~ "Public sector health facility",
+    m15 %in% 30:39 ~ "Private medical sector (non_NGO) health facility",
+    m15 %in% 40:49 ~ "Private medical sector (NGO) health facility",
+    m15 %in% 10:12 ~ "At home",
+    m15 %in% c(13, 14, 96) ~ "Other"
+  ))
+
+##as numeric for birthweight
+##9996 is not weighed and 9998 is don't no, will convert to NAs for analysis
+births_last3years$m19 <- as.numeric(births_last3years$m19)
+births_last3years <- births_last3years %>%
+  mutate(m19 = ifelse(m19 %in% c(9996, 9998), NA, m19))
+
+##divide bmi v445 by 100 and 9998 as flagged
+births_last3years$v445 <- as.numeric(births_last3years$v445)/100
+
+##catagorise ANC to 0, 1-3, 4-6, 7+ (m14)? can change this!
+births_last3years<- births_last3years %>%
+  mutate(ANC_visits = case_when(
+    m14 == 0 ~ "None",
+    between(m14, 1, 3) ~ "1-3",
+    between(m14, 4, 6) ~ "4-6",
+    m14 >= 7 ~ "7+",
+    TRUE ~ NA_character_  # For any other cases, set to NA
+  ))
+
+
+##remove uneeded recoded variables
+births_clean <- births_last3years %>% 
+  select(-v011, -v008, -b3, -m3a, -m3b, -m3c, -m3d
+         , -m3e, -m3f, -m3g, -m3h, -m3i, -m3k, -m3n
+         , - test, -v012, -b6, -b7, -m14)
+
+##reorder dataframe
+births_clean <- births_clean %>%
+  select(1:7, mum_age_pregnancy, b5, neo_mort, age_at_death_days, b4, b0, b20, b11, b12,
+         m13, ANC_visits, m15, senior_delivery_attendant, m17, m19, m45, m66, m70, s1125, everything())
+
+##DATA DICTIONARY
+
+#births_clean data dictionary 
+variable_names_BR_clean <- names(births_clean)
+variable_labels_BR_clean <- sapply(births_clean, function(x) {
+  lbl <- attr(x, "label")
+  if (is.null(lbl)) "No label" else lbl
+})
+
+dict_births_BR <- data.frame(
+  variable = variable_names_BR_clean,
+  description = variable_labels_BR_clean
+)
+
+##CALCULATIONS
+##how many neonatal deaths?
+sum(births_clean$neo_mort == "Yes")
+
+## table of unweighted frequencies for neonatal deaths in rural and urban areas
+tbl <- table(births_clean$neo_mort,births_clean$v025)
+
+##proportions by rural and urban births(unweighted)
+proportions(tbl, margin = 2)
+
+#see DHS weighted statistics, need to divide by 1000000
+births_clean$wt <- births_clean$v005/1000000
+births_clean <- births_clean %>%
+  mutate(urban_rural = factor(v025, levels = c(1, 2), labels = c("Urban", "Rural")))
+
+
+####################### RATES OF NEONATAL MORTALITY ############################
+#Add v024 to "births_clean"
+
+#Setting up survey package 
+births_clean$wt <- births_clean$v005/1000000
+
+design <- survey::svydesign(id=~v001, 
+                            strata =~v023, 
+                            weights=~wt,
+                            data= births_clean)
+
+nm_strata_raw <- svyby(~(neo_mort == "Yes"), ~v024 +v025, design, svymean, na.rm = TRUE)
+nm_strata <- nm_strata_raw[,c(1,2,4,6)] 
+nm_strata <- nm_strata %>% mutate (nm = nm_strata[,3]*100)
+View(nm_strata)
+write.csv(nm_strata, "nm_strata.csv", row.names = T)
